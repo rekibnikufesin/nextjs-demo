91b7954255cba3d408e93ffb63672032384ef6aa
Will Button <will@willbutton.co>
2022-07-21 15:51:09 -0700

    implement Cloudflare DNS

--- infra/index.js
+++ infra/index.js
@@ -1,10 +1,12 @@
 "use strict";
 const aws = require("@pulumi/aws")
+const cloudflare = require("@pulumi/cloudflare")
 const pulumi = require("@pulumi/pulumi");
+const siteDomain = "opswod.io"
 
 // Create an S3 bucket
-let siteBucket = new aws.s3.Bucket("opswod.io", {
-  name: "opswod.io",
+const siteBucket = new aws.s3.Bucket("opswod.io", {
+  name: siteDomain,
   website: {
     indexDocument: "index.html"
   }
@@ -34,9 +36,31 @@ function publicReadPolicyForBucket(bucketName) {
   })
 }
 
-let bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
+// S3 bucket policy
+const bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
   bucket: siteBucket.bucket,
   policy: siteBucket.bucket.apply(publicReadPolicyForBucket)
+})
+
+// Cloudflare DNS
+const cloudflareZone = cloudflare.Zone.get(siteDomain)
+
+const rootDNS = new cloudflare.Record("rootDNS", {
+  name: siteDomain,
+  zoneId: cloudflareZone.zoneId,
+  type: "CNAME",
+  value: websiteUrl,
+  ttl: 1,
+  proxied: true,
+})
+
+const wwwDNS = new cloudflare.Record("wwwDNS", {
+  name: "www",
+  zoneId: cloudflareZone.zoneId,
+  type: "CNAME",
+  value: siteDomain,
+  ttl: 1,
+  proxied: true,
 })
 
 exports.websiteUrl = siteBucket.websiteEndpoint
